269c2e11e829a6e25f82e5b15d58783c1ec0d869
Paulo Toro <egosumscientia@gmail.com>
2025-12-06 17:16:43 -0500

    update

--- infra/providers.tf
+++ infra/providers.tf
@@ -35,26 +35,3 @@ provider "google-beta" {
   region      = var.region
   zone        = var.zone
 }
-
-
-####################################################
-# (Opcional, recomendado)
-# Habilitar APIs necesarias del proyecto vía Terraform
-####################################################
-# Esta sección la activaremos en "main.tf":
-#
-#  - container.googleapis.com        (GKE)
-#  - sqladmin.googleapis.com         (Cloud SQL)
-#  - pubsub.googleapis.com           (Pub/Sub)
-#  - artifactregistry.googleapis.com (Artifact Registry)
-#
-# module "enable_apis" {
-#   source  = "terraform-google-modules/project-factory/google//modules/project_services"
-#   version = "~> 14.0"
-#
-#   project_id = var.project_id
-#   activate_apis = [
-#     "container.googleapis.com",
-#     "sqladmin.googleapis.com",
-#     "pubsub.googleapis.com",
-#     "art
